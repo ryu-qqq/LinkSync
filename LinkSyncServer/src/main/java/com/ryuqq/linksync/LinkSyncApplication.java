f4ddc56652a9ccd1b49b1d9daecf7901b0274a15
RyuSangwon <rsw2@connectly.co.kr>
2024-10-04 00:46:37 +0900

    Initial commit with both LinkSyncServer and LinkSyncPipeLine

---
+++ LinkSyncServer/src/main/java/com/ryuqq/linksync/LinkSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ryuqq.linksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSyncApplication.class, args);
+    }
+
+}
---
+++ LinkSyncServer/src/test/java/com/ryuqq/linksync/LinkSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryuqq.linksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
